6ccf146ee24c27fa9e98d6f4e0217a8c603008d7
ADMIN <ADMIN@DESKTOP-B0D0J2Q>
2024-04-17 21:44:07 +0700

    Add project files.

---
+++ pbl/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pbl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+                    }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool isClick = false;
+            if (isClick)
+            {
+                panel4.Size = panel4.MinimumSize;
+                isClick = true;
+            }
+            else
+            {
+                panel4.Size = panel4.MaximumSize;
+                isClick = false;
+            }
+        }
+    }
+}
